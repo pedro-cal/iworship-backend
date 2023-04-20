96e7e76bd666aba71e6e93c7b142f73652ed44ef
admin <admin@MacBook-Air-de-admin.local>
2023-04-20 11:10:40 -0300

    code: remove unwanted console logs

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,8 @@ app.get('/songs', async (req, res) => {
    await mongoClient.connect();
    const db = mongoClient.db("iworshipDB");
    const songsCursor = db.collection('songs').find({});
-   // console.log('songs cursor:', songs);
-   // let songs = [];
+
    const songs = await songsCursor.map((doc) => (doc)).toArray();
-   console.log('songs', songs);
    res.json({ songs })
 });
 
